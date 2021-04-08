29d369887671a47dd1d7425922fd1756c418ec0a
ZhouChenghan <501276687@qq.com>
2021-04-08 11:38:01 +0200

    data and instr mem

--- Design/Data Path/components/project_1/project_1.srcs/sources_1/new/data_mem.vhd
+++ Design/Data Path/components/project_1/project_1.srcs/sources_1/new/data_mem.vhd
@@ -37,16 +37,27 @@ entity data_mem is
     nbit_data : natural :=31
     );
     port (
+    clk: in std_logic;
     data_in: in std_logic_vector(nbit_data downto 0);
-    WR: in std_logic;
-    data_add: in std_logic_vector(nbit_address downto 0);
+    wr: in std_logic;
+    data_addr_in: in integer range nbit_address downto 0;--什么时候用什么样的变量不是很懂
+    data_addr_out: in integer range nbit_address downto 0;
     data_out: out std_logic_vector(nbit_data downto 0)
     );
 end data_mem;
 
 architecture Behavioral of data_mem is
-
+    type mem is array(63 downto 0) of std_logic_vector(nbit_data downto 0);
+    signal ram_block : mem;
 begin
-
-
+    process (clk)
+    begin
+        if (clk='1' and clk'event) then
+            if (wr='1') then
+                ram_block(data_addr_in)<=data_in;--write
+            else
+                data_out<=ram_block(data_addr_out);--read
+            end if;
+        end if; 
+    end process;
 end Behavioral;
--- Design/Data Path/components/project_1/project_1.srcs/sources_1/new/ins_mem.vhd
+++ Design/Data Path/components/project_1/project_1.srcs/sources_1/new/ins_mem.vhd
@@ -37,14 +37,20 @@ entity ins_mem is
     nbit_data : natural :=31
     );
     port (
-    data_add: in std_logic_vector(nbit_address downto 0);
+    clk: in std_logic;
+    data_addr_out: in integer range nbit_address downto 0;
     data_out: out std_logic_vector(nbit_data downto 0)
     );
 end ins_mem;
 
 architecture Behavioral of ins_mem is
-
+type mem is array(63 downto 0) of std_logic_vector(32 downto 0);  
+signal rom_mem: mem;
 begin
-
-
+    process(clk)
+    begin
+        if (clk'event and clk='1') then
+            data_out<=rom_mem(data_addr_out);
+        end if; 
+    end process;
 end Behavioral;
